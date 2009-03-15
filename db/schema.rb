067262299a00fa0f242c0b5de51b4d52e30f390d
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-03-15 16:25:14 +0500

    fix schema to be in sync with migrations

--- db/schema.rb
+++ db/schema.rb
@@ -9,15 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 9) do
+ActiveRecord::Schema.define(:version => 20081224020432) do
 
   create_table "comments", :force => true do |t|
-    t.integer  "post_id",                 :null => false
-    t.string   "author",                  :null => false
-    t.string   "author_url",              :null => false
-    t.string   "author_email",            :null => false
-    t.text     "body",                    :null => false
-    t.text     "body_html",               :null => false
+    t.integer  "post_id",      :null => false
+    t.string   "author",       :null => false
+    t.string   "author_url",   :null => false
+    t.string   "author_email", :null => false
+    t.text     "body",         :null => false
+    t.text     "body_html",    :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
