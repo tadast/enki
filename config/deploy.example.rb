d7fb74a2f2abc85f12e91138292a892ecb287668
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-03-15 17:25:53 +0500

    added example capistrano deploy configs & scripts

---
+++ config/deploy.example.rb
@@ -0,0 +1,36 @@
+set :deploy_via, :remote_cache
+set :use_sudo, false
+set :application, "enki"
+set :scm, :git
+set :repository, "git://github.com/zed-0xff/enki.git"
+set :deploy_to, "/PATH/TO/DEPLOY/#{application}"
+set :user, 'USER_TO_DEPLOY_AS'
+
+server "SERVER_TO_DEPLOY_TO", :app, :web, :db, :primary => true
+
+shared_configs = %w'database.yml mongrel_cluster.yml'
+
+namespace :deploy do
+  task :symlink_shared_configs do
+    shared_configs.each do |config|
+      run "ln -s #{shared_path}/config/#{config} #{latest_release}/config/"
+    end
+
+    run "ln -s #{shared_path}/locks #{latest_release}/"
+  end
+
+  task :restart do
+    run "mongrel_rails cluster::restart -C #{latest_release}/config/mongrel_cluster.yml"
+  end
+
+  task :start do
+    run "mongrel_rails cluster::start -C #{latest_release}/config/mongrel_cluster.yml"
+  end
+
+  task :stop do
+    run "mongrel_rails cluster::stop -C #{latest_release}/config/mongrel_cluster.yml"
+  end
+
+end
+
+after "deploy:finalize_update", "deploy:symlink_shared_configs"
