02b83ed6421a88e00fb890b6c9d7a58b55c72403
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-05-23 15:00:59 +0600

    fix google analytics workaround

--- config/initializers/google_analytics_workaround.rb
+++ config/initializers/google_analytics_workaround.rb
@@ -1,6 +1,6 @@
 # workaround for the case when Rubaidh::GoogleAnalytics is loaded,
 # but tracker_id is not specified.
 # without this workaround it will produce Rubaidh::GoogleAnalyticsConfigurationError
-if defined?Rubaidh && defined?Rubaidh::GoogleAnalytics && !Rubaidh::GoogleAnalytics.tracker_id
+if defined?(Rubaidh) && defined?(Rubaidh::GoogleAnalytics) && !Rubaidh::GoogleAnalytics.tracker_id
   Rubaidh::GoogleAnalytics.tracker_id = 'UA-CHANGE-ME'
 end
