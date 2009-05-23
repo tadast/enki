3a400fedad4f6bf3894ba94b1231993518266806
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-05-23 14:26:35 +0600

    use blog domain as domain part of exception notifier messages FROM field

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,1 +1,3 @@
 ExceptionNotifier.exception_recipients = [Enki::Config.default[:author, :email]]
+ExceptionNotifier.sender_address = 
+  %("Application Error" <app.error@#{URI.parse(Enki::Config.default[:url]).host}>)
