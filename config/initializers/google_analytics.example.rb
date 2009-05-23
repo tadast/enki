18ec2a547248a972ccf9984d086a39cc4608be48
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-05-23 15:48:15 +0600

    google analytics comment fix

--- config/initializers/google_analytics.example.rb
+++ config/initializers/google_analytics.example.rb
@@ -1,4 +1,4 @@
-if defined?Rubaidh && defined?Rubaidh::GoogleAnalytics
-  # replace UA-CHANGE-ME with your own tracker_id
-  Rubaidh::GoogleAnalytics.tracker_id = 'UA-CHANGE-ME'
+if defined?(Rubaidh) && defined?(Rubaidh::GoogleAnalytics)
+  ## uncomment next string and replace UA-CHANGE-ME with your own tracker_id
+  #Rubaidh::GoogleAnalytics.tracker_id = 'UA-CHANGE-ME'
 end
