330b5e43b180e898fdb29ec63403891763bf134e
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-10-12 15:07:36 +0600

    use 'yandex_metrika' gem instead of static Yandex.Metrika code

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
   config.gem "rutils"
   config.gem "chrislloyd-gravtastic", :lib => false, :source => "http://gems.github.com"
   config.gem "rubaidh-google_analytics", :lib => false, :source => "http://gems.github.com"
+  config.gem "yandex_metrika", :lib => "yandex/metrika", :source => "http://gemcutter.org"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -45,3 +46,5 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+Yandex::Metrika.counter_id = 221066 if defined?Yandex::Metrika
