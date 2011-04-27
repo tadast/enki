48b86fb6acab685d6a64a7f68972e4f2273ec1cd
Andrey 'Zed' Zaikin <zed.0xff@gmail.com>
2011-04-27 21:59:34 +0400

    upd to rails 3.0.7

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,5 +4,5 @@ Enki::Application.config.session_store :cookie_store, :key => '_enki_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
+# (create the session table with "rails generate session_migration")
 # Enki::Application.config.session_store :active_record_store
