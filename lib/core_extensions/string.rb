d7dac4bc0f9c2cf2ad02ffde56c70f073a58528d
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2011-04-29 00:52:26 +0400

    return back russian titles2slugs transliteration

--- lib/core_extensions/string.rb
+++ lib/core_extensions/string.rb
@@ -1,10 +1,11 @@
 module CoreExtensions
   module String
-  
+
     def slugorize
       result = self.downcase
       result.gsub!(/&([0-9a-z#])+;/, '')  # Ditch Entities
       result.gsub!('&', 'and')            # Replace & with 'and'
+      result.translify! if result.respond_to?(:translify!)
       result.gsub!(/[^a-z0-9\-']/, '-')   # Get rid of anything we don't like
       result.gsub!(/-+/, '-')             # collapse dashes
       result.gsub!(/-$/, '')              # trim dashes
